cee59b75151502771c5e7a395c027942614d4907
vfernandez33 <vfernandez@povertyactionlab.org>
2025-06-05 16:41:12 -0600

    Add files via upload

---
+++ tldraw.umd.js
@@ -0,0 +1,6 @@
+
+window.tldraw = {
+  createApp: function (el) {
+    el.innerHTML = '<div style="text-align:center;margin-top:80px;"><h2 style="color:#319f90;">Editor IMPACTA</h2><p style="font-size:18px;">Esta es una versión de respaldo. El editor personalizado estará disponible mañana.</p></div>';
+  }
+};
